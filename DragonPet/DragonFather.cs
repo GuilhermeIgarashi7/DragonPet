2e2a70ffb568128b03ba41ec10f0c674ba94f25a
Guilherme Igarashi <gui.igarashi@hotmail.com>
2024-04-04 17:48:32 -0300

    Falta o CS

--- DragonPet/DragonFather.cs
+++ DragonPet/DragonFather.cs
@@ -2,5 +2,15 @@ namespace DragonFather;
 
     public class DragonFather
     {
-        
+        protected double hungry;
+        protected double thirst;
+        protected double happiness;
     }
+//---------------------------------------------------------------------------------------------------
+    public DragonFather()
+    {
+        hungry = 0.2;
+        thirst = 0.4;
+        happiness = 0.1;
+    }
+//----------------------------------------------------------------------------------------------------
